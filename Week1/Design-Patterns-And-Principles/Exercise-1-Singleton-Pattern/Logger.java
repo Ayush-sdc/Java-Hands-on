7fa9efcbd6b90d73989dd2d49ebcf48ab62aebf0
Ayush Prajapati <er.ayushprajapati@gmail.com>
2026-06-23 07:37:45 +0530

    Update Logger.java

--- Week1/Design-Patterns-And-Principles/Exercise-1-Singleton-Pattern/Logger.java
+++ Week1/Design-Patterns-And-Principles/Exercise-1-Singleton-Pattern/Logger.java
@@ -1,17 +1,17 @@
 public class Logger {
 
-    // Creating a static variable to store the single instance
+    
     private static Logger instance;
 
-    // Private constructor so object cannot be created outside
+    
     private Logger() {
         System.out.println("Logger object created.");
     }
 
-    // Method to get the single instance
+    
     public static Logger getInstance() {
 
-        // Create object only if it doesn't exist
+        
         if (instance == null) {
             instance = new Logger();
         }
@@ -19,7 +19,7 @@ public class Logger {
         return instance;
     }
 
-    // Simple logging method
+    
     public void log(String message) {
         System.out.println("LOG: " + message);
     }
